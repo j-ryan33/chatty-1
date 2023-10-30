af96d2f29f92cdda7c25ad62a906bc552e986d27
j-ryan33 <148741424+j-ryan33@users.noreply.github.com>
2023-10-30 15:33:19 -0700

    attempt to run his example (functions.js)

--- public_html/functions.js
+++ public_html/functions.js
@@ -1,68 +1,77 @@
-const { text } = require("express");
 
+selectedCat = 'Work';
 
-function logmexoxo(){
-    console.log("julia!!!!!!!!!!!!! :D");
+function getCategories() {
+  let url = '/categories/';
+  let p = fetch(url);
+  let ps = p.then( (response) => {
+    return response.json();
+  }).then((objects) => { 
+    let html = '';
+    for (i in objects) {
+      html += '<div onclick="getItemsForCategory(\'' + objects[i].name + '\')" style="cursor:pointer;color:' + objects[i].color + '">' + objects[i].name + '</div>\n'
+    }
+    let x = document.getElementById('categories');
+    x.innerHTML = html;
+  }).catch(() => { 
+    alert('something went wrong');
+  });
 }
 
-
-
-
-function displayNewMessage(name, message){
-    /*
-    Parameters:    
-        -  name (string): who sent the message
-        -  message (string): message content
-    */
-   console.log("#0!");
-    const displaytxt = document.getElementById("conversation-text-container");
-    console.log("#1!");
-    
-    const msgContainerElement = document.createElement("one-message-container");
-    console.log("#2!");
-    const nameElement = document.createElement("alias-display-text");
-    console.log("#3!");
-    nameElement.innerHTML = name + ": ";
-    console.log("#4!");
-    const messageElement = document.createElement("message-display-text");
-    messageElement.innerHTML = message;
-    console.log("#5!");
-    msgContainerElement.appendChild(nameElement);
-    msgContainerElement.appendChild(messageElement);
-    console.log("#6!");
-
-    displaytxt.appendChild(msgContainerElement);
-    console.log("#7!");
-
-
-    // 
+function getItemsForCategory(cat) {
+  selectedCat = cat;
+  let url = '/items/' + cat;
+  let p = fetch(url);
+  let ps = p.then( (response) => {
+    return response.json();
+  }).then((objects) => { 
+    let html = '';
+    for (i in objects) {
+      html += '<div>' + objects[i].name + ' - ' + objects[i].stat + '</div>\n'
+    }
+    let x = document.getElementById('items');
+    x.innerHTML = html;
+  }).catch(() => { 
+    alert('something went wrong');
+  });
 }
 
-displayNewMessage("Julia", "Hello, who is THIS?");
-
-
-function allMessages(){
-    let url = "http://localhost/chats"
-    fetch(url).then((response) => {
-        return response.text();
-    }).then((text) => {
-        document.getElementById("conversation-text-container").innerHTML = text;
-    }).catch((error) => {
-        console.log("ERROR: " + error);
-    });
+function createCategory() {
+  let n = document.getElementById('catName').value;
+  let pr = document.getElementById('catPrio').value;
+  let c = document.getElementById('catColor').value;
+  let url = '/create/category/';
+  let data = { name: n, priority: pr, color: c};
+  let p = fetch(url, {
+    method: 'POST', 
+    body: JSON.stringify(data),
+    headers: {"Content-Type": "application/json"}
+  });
+  p.then(() => {
+    console.log('Category Created');
+    getCategories();
+  });
+  p.catch(() => { 
+    alert('something went wrong');
+  });
 }
 
-function newMessage(){
-    var newAlias = document.getElementById("alias-in").value;
-    var newMessage = document.getElementById("message-in").value;
-    let url = "http://localhost/chats/post";
-    fetch("/chats/post" + new URLSearchParams ({
-        alias: newAlias,
-        message: newMessage
-    }))
-    
-
-    
-    
-
+function createItem() {
+  let n = document.getElementById('itemName').value;
+  let s = document.getElementById('itemStat').value;
+  let d = document.getElementById('itemDifficulty').value;
+  let url = '/create/item/';
+  let data = { name: n, stat: s, difficulty: d, category: selectedCat};
+  let p = fetch(url, {
+    method: 'POST', 
+    body: JSON.stringify(data),
+    headers: {"Content-Type": "application/json"}
+  });
+  p.then(() => { 
+    getItemsForCategory(selectedCat);
+  });
+  p.catch(() => { 
+    alert('something went wrong');
+  });
 }
+
