002f704484e827b06f8c843486dd48ddf1aa789a
j-ryan33 <148741424+j-ryan33@users.noreply.github.com>
2023-10-30 18:52:33 -0700

    Update functions.js

--- public_html/functions.js
+++ public_html/functions.js
@@ -19,67 +19,15 @@ function getUsers() {
     for (i in objects) {
       html += '<div>' + objects[i] + '</div>\n'
     }
-    let x = document.getElementById('categories');
-    x.innerHTML = html;
+    let bHTML = document.getElementById('bodyContain');
+    bHTML.innerHTML = html;
   }).catch(() => { 
     alert('something went wrong');
   });
 }
 
-function getItemsForCategory(cat) {
-  selectedCat = cat;
-  let url = '/items/' + cat;
-  let p = fetch(url);
-  let ps = p.then( (response) => {
-    return response.json();
-  }).then((objects) => { 
-    let html = '';
-    for (i in objects) {
-      html += '<div>' + objects[i].name + ' - ' + objects[i].stat + '</div>\n'
-    }
-    let x = document.getElementById('items');
-    x.innerHTML = html;
-  }).catch(() => { 
-    alert('something went wrong');
-  });
-}
-
-function createCategory() {
-  let n = document.getElementById('catName').value;
-  let pr = document.getElementById('catPrio').value;
-  let c = document.getElementById('catColor').value;
-  let url = '/create/category/';
-  let data = { name: n, priority: pr, color: c};
-  let p = fetch(url, {
-    method: 'POST', 
-    body: JSON.stringify(data),
-    headers: {"Content-Type": "application/json"}
-  });
-  p.then(() => {
-    console.log('Category Created');
-    getCategories();
-  });
-  p.catch(() => { 
-    alert('something went wrong');
-  });
-}
 
 function createItem() {
-  let n = document.getElementById('itemName').value;
-  let s = document.getElementById('itemStat').value;
-  let d = document.getElementById('itemDifficulty').value;
-  let url = '/create/item/';
-  let data = { name: n, stat: s, difficulty: d, category: selectedCat};
-  let p = fetch(url, {
-    method: 'POST', 
-    body: JSON.stringify(data),
-    headers: {"Content-Type": "application/json"}
-  });
-  p.then(() => { 
-    getItemsForCategory(selectedCat);
-  });
-  p.catch(() => { 
-    alert('something went wrong');
-  });
+  
 }
 
