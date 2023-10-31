5111b679b00f4449d7ed76f4d06d14eb6dbb7154
j-ryan33 <148741424+j-ryan33@users.noreply.github.com>
2023-10-30 18:38:30 -0700

    UPDATED server.js

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const fs = require('fs');
-const crypto = require('crypto');
 
-const connection_string = 'mongodb+srv://doadmin:94B6u27tbJX1P83Q@chatty-db-2b2a9a25.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=chatty-db';
+const connection = 'mongodb+srv://doadmin:94B6u27tbJX1P83Q@chatty-db-2b2a9a25.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=chatty-db';
 
-mongoose.connect(connection_string);
+mongoose.connect(connection);
 mongoose.connection.on('error', () => {
-  console.log('There was a problem connecting to mongoDB');
+  console.log('Connection issue with mongoDB :(');
 });
 
-/**
- * Specify the schema for the database, including items and categories.
- */
-var ItemSchema = new mongoose.Schema( {
-    name: String,
-    difficulty: Number,
-    stat: String,
-    category: String
+var Users = new mongoose.Schema( {
+  username: String,
+  password: String,
+  // listings: 
+  // purchases: 
 });
-var Item = mongoose.model('Item', ItemSchema);
-var CategorySchema = new mongoose.Schema( {
-  name: String,
-  priority: Number,
-  color: String
-});
-var Category = mongoose.model('Category', CategorySchema);
 
-/**
- * Initialize the express app and configure with various features 
- * such as JSON parsing, static file serving, etc.
- */
+var User = mongoose.model('User', Users);
+
+var Items = new mongoose.Schema( {
+    title: String,
+    disc: String,
+    image: String,
+    price: Number,
+    status: String,
+    username: String
+});
+
+var Item = mongoose.model('Item', Items);
+
+
 const app = express();
-app.use(cookieParser());    
+// app.use(cookieParser());    
 app.use(express.static('public_html'))
 //app.get('/', (req, res) => { res.redirect('/app/index.html'); });
 app.use(express.json())
@@ -43,79 +42,53 @@ app.use(express.json())
  * Get all of the items that correspond with a particular catagory.
  * The category is specified as a part of the URL after /items/.
  */
-app.get('/items/:cat', (req, res) => {
-    let cat = req.params.cat;
-    let p1 = Item.find({category: cat}).exec();
-    p1.then( (results) => { 
+
+
+app.get('/get/users/', (req, res) => {
+    let all = User.find({}).exec();
+    all.then((results) => { 
+      res.end(JSON.stringify(results));
+    });
+    all.catch((error) => {
+      console.log(error);
+      res.end('NO GOOD.');
+    });
+});
+
+app.get('/get/items/', (req, res) => {
+    let all = Item.find({}).exec();
+    all.then((results) => { 
+      res.end(JSON.stringify(results));
+    });
+    all.catch((error) => {
+      console.log(error);
+      res.end('NO GOOD.');
+    });
+});
+
+app.get('/get/purchases/:usrnm', (req, res) => {
+    let usrnm = req.params.usrnm;
+    let usr = Item.find({category: cat}).exec();
+    usr.then( (results) => { 
       res.end( JSON.stringify(results) );
     });
-    p1.catch( (error) => {
+    usr.catch( (error) => {
       console.log(error);
       res.end('FAIL');
     });
 });
 
-app.get('/testing', (req, res, next) => {
-  console.log('A');
-  next();
-});
-
-app.get('/testing', (req, res, next) => {
-  console.log('B');
-  next();
-});
-
-app.get('/testing', (req, res) => {
-  console.log('C');
-  res.end('hi')
-});
-
-
-/**
- * This route is for creating a new item for a particular category. 
- * The category that the items belongs to is stored as a part of the item object in the database.
- */
-app.post('/create/item/', (req, res) => {
-  let newItemToSave = req.body;
-  var newItem = new Item(newItemToSave);
-  let p1 = newItem.save();
-  p1.then( (doc) => { 
+app.post('/add/user/', (req, res) => {
+  let userToBeSaved = req.body;
+  var newUser = new User(userToBeSaved);
+  let newUsr = newUser.save();
+  newUsr.then( (doc) => { 
     res.end('SAVED SUCCESFULLY');
   });
-  p1.catch( (err) => { 
-    console.log(err);
-    res.end('FAIL');
+  newUsr.catch( (err) => { 
+    console.log("COULD NOT CREATE USER!!!");
+    res.end("COULD NOT CREATE USER!!!");
   });
-});
-
-/**
- * This route is for creating a new list category.
- */
-app.post('/create/category/', (req, res) => {
-  let CategoryToSave = req.body;
-  var newCategory = new Category(CategoryToSave);
-  let p1 = newCategory.save();
-  p1.then( (doc) => { 
-    res.end('SAVED SUCCESFULLY');
-  });
-  p1.catch( (err) => { 
-    console.log(err);
-    res.end('FAILED TO CREATE A CATEGORY');
-  });
-});
-
-/**
- * This route is for fetching all of the categories stored in the database.
- */
-app.get('/categories/', (req, res) => {
-    let p1 = Category.find({}).exec();
-    p1.then( (results) => { 
-      res.end( JSON.stringify(results) );
-    });
-    p1.catch( (error) => {
-      console.log(error);
-      res.end('FAIL');
-    });
 });
 
 // Start up the server to listen on port 80
