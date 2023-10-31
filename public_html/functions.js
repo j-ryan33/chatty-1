f22ce6fa4042a38e0833cd42824145dda82adeba
j-ryan33 <148741424+j-ryan33@users.noreply.github.com>
2023-10-30 18:57:38 -0700

    Update functions.js

--- public_html/functions.js
+++ public_html/functions.js
@@ -1,33 +1,32 @@
-
-function createAccount(){
-  let usr = document.getElementById('usrName').value;
-  let pass = document.getElementById('password').value;
-  let p = fetch('/add/user/' + usr + "/" + encodeURIComponent(pass);
-  p.then((response) =>{
-    return response.text();
-  }).then((text) => {
-    alert(text);
-  });
-
-
-function getUsers() {
-  let p = fetch('/get/users/');
-  let rp = p.then( (response) => {
-    return response.json();
-  }).then((objects) => { 
-    let html = '';
-    for (i in objects) {
-      html += '<div>' + objects[i] + '</div>\n'
-    }
-    let bHTML = document.getElementById('bodyContain');
-    bHTML.innerHTML = html;
-  }).catch(() => { 
-    alert('something went wrong');
-  });
-}
-
-
-function createItem() {
+function createUser(){
+    let usr = document.getElementById('usrName').value;
+    let pass = document.getElementById('password').value;
+    let p = fetch('/add/user/' + usr + "/" + encodeURIComponent(pass));
+    p.then((response) =>{
+      return response.text();
+    }).then((text) => {
+      alert(text);
+    });
   
-}
+  
+  function getUsers() {
+    let p = fetch('/get/users/');
+    let rp = p.then( (response) => {
+      return response.json();
+    }).then((objects) => { 
+      let html = '';
+      for (i in objects) {
+        html += '<div>' + objects[i] + '</div>\n'
+      }
+      let bHTML = document.getElementById('bodyContain');
+      bHTML.innerHTML = html;
+    }).catch(() => { 
+      alert('something went wrong');
+    });
+  }
+
+
+//function createItem() {
+  
+//}
 
