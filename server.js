a07ee6396b38bc86d9f380df48ab77c633c5804b
j-ryan33 <148741424+j-ryan33@users.noreply.github.com>
2023-11-08 01:06:01 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 
 // const connection = 'mongodb+srv://doadmin:94B6u27tbJX1P83Q@chatty-db-2b2a9a25.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=chatty-db';
-const connection = 'mongodb:';
+const connection = 'mongodb+srv://doapps-01f70429-fac0-4ba6-83ed-369febb34dfd:H864cjg7BG91Mp32@chatty-db-2b2a9a25.mongo.ondigitalocean.com/admin?authSource=admin&tls=true';
 
 
 mongoose.connect(connection);
