087e966445e4349e4072f582ef700dddcc6067e1
j-ryan33 <148741424+j-ryan33@users.noreply.github.com>
2023-11-08 11:55:56 -0700

    Add files via upload

---
+++ public_html/oosta_functions.js
@@ -0,0 +1,34 @@
+function createUser(){
+    let usr = document.getElementById('usrName').value;
+    let pass = document.getElementById('password').value;
+    let p = fetch('/add/user/' + usr + "/" + encodeURIComponent(pass));
+    p.then((response) =>{
+      return response.text();
+    }).then((text) => {
+      alert(text);
+    });
+  }
+
+  
+  
+  function getUsers() {
+    let url = '/get/users/'
+    let p = fetch(url);
+    let rp = p.then( (response) => {
+      return response.json();
+    }).then((objects) => { 
+      let html = '';
+      for (i in objects) {
+        html += '<div>' + objects[i] + '</div>\n'
+      }
+      let bHTML = document.getElementById('bodyContain');
+      bHTML.innerHTML = html;
+    }).catch(() => { 
+      alert('something went wrong');
+    });
+  }
+  
+  
+//   function createItem() {
+//     pass;
+//   }
