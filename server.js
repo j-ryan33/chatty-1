5b1506654aa4d128f1684d1f5b2162dc4b4fc413
j-ryan33 <148741424+j-ryan33@users.noreply.github.com>
2023-11-08 17:07:01 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 // const cookieParser = require('cookie-parser');
 const app = express();
 const router = express.Router();
+const bp = require('body-parser');
+const { use } = require('express/lib/application');
 
 
 // var ejs = require('ejs');
@@ -39,21 +41,21 @@ var Users = new mongoose.Schema( {
 
 var User = mongoose.model('User', Users);
 
-let myNewUser = new User({
-  username: "helloooothisisaUSER",
-  password: "passwordy-word",
-  listings: [9102842093, 109283],
-  purchases: [52348782, 90821232, 293239, 203293]
-});
+// let myNewUser = new User({
+//   username: "helloooothisisaUSER",
+//   password: "passwordy-word",
+//   listings: [9102842093, 109283],
+//   purchases: [52348782, 90821232, 293239, 203293]
+// });
 
 
-let p = myNewUser.save();
-p.then(() => {
-  console.log('CREATED SUCCESSFULLY');
-});
-p.catch(() => {
-  console.log("no save :(")
-});
+// let p = myNewUser.save();
+// p.then(() => {
+//   console.log('CREATED SUCCESSFULLY');
+// });
+// p.catch(() => {
+//   console.log("no save :(")
+// });
 
 var Items = new mongoose.Schema( {
     title: String,
@@ -61,14 +63,46 @@ var Items = new mongoose.Schema( {
     image: String,
     price: Number,
     status: String,
-    username: String
+    sellerUsername: String,
+    buyerUsername: String
 });
 
 var Item = mongoose.model('Item', Items);
 
+// let myNewItem = new Item({
+//     title: "A ",
+//     disc: "It's a truck and has 4 wheels. Goes vroom fr fr",
+//     // image: ,
+//     price: 1000000000,
+//     status: "SALE",
+//     sellerUsername: "iwoidjwoidw",
+//     buyerUsername: "[NONE]"
+//     // sold: Boolean
+// });
+
+
+/*
+when an item is bought, use the ".then" to
+get it to add it to the user's "purchases" 
+list. 
+*/
+
+
+// let p = myNewItem.save();
+// p.then(() => {
+//   console.log('ITEM CREATED SUCCESSFULLY!!!');
+//   // THEN MODIFY THE "purchases" FIELD IN USER SCHEMA
+
+// });
+// p.catch(() => {
+//   console.log("no save :(")
+// });
+
+
 // app.get('/', function (req, res) {
 //   res.render('index.html', {});
 // });
+
 
 
 app.get('/', function (req, res) {
@@ -94,6 +128,11 @@ app.get('/index', function (req, res) {
   res.sendFile(__dirname + '/public_html/index.html');
 });
 
+app.get('/index.html', function (req, res) {
+  res.sendFile(__dirname + '/public_html/index.html');
+
+});
+
 app.get('/login', function (req, res) {
   res.sendFile(__dirname + '/public_html/index.html');
 });
@@ -102,7 +141,15 @@ app.get('/home', function (req, res) {
   res.sendFile(__dirname + '/public_html/home.html');
 });
 
+app.get('/home.html', function (req, res) {
+  res.sendFile(__dirname + '/public_html/home.html');
+});
+
 app.get('/post', function (req, res) {
+  res.sendFile(__dirname + '/public_html/post.html');
+});
+
+app.get('/post.html', function (req, res) {
   res.sendFile(__dirname + '/public_html/post.html');
 });
 
@@ -136,18 +183,30 @@ app.get('/get/items/', (req, res) => {
     });
 });
 
-app.get('/get/purchases/:usrnm', (req, res) => {
-    let usrnm = req.params.usrnm;
-    let usr = Item.find({category: cat}).exec();
-    usr.then( (results) => { 
-      res.end( JSON.stringify(results) );
-    });
-    usr.catch( (error) => {
-      console.log(error);
-      res.end('FAIL');
-    });
+app.get('/get/user/:username'), (req, res) =>{
+  let usrnm = req.params.username;
+  let usr = User.find({ username: usrnm});
+  usr.then( (results) => { 
+    res.end( JSON.stringify(results) );
+  });
+  usr.catch( (error) => {
+    console.log(error);
+    res.end('FAIL');
+  });
+}
+
+// app.get('/get/purchases/:usrnm', (req, res) => {
+//     let usrnm = req.params.usrnm;
+//     let usr = Item.find({category: cat}).exec();
+//     usr.then( (results) => { 
+//       res.end( JSON.stringify(results) );
+//     });
+//     usr.catch( (error) => {
+//       console.log(error);
+//       res.end('FAIL');
+//     });
     
-});
+// });
 
 app.post('/add/user/', (req, res) => {
   let userToBeSaved = req.body;
@@ -165,7 +224,105 @@ app.post('/add/user/', (req, res) => {
 });
 
 
+app.post('/add/item/', (req, res) => {
+  let itemToBeSaved = req.body;
+  // parameter=value&also=another
+  var newItem = new Item(userToBeSaved);
+  let itemSaved = newItem.save();
+  itemSaved.then( (doc) => { 
+    res.end('SAVED SUCCESFULLY');
+    /*
+    !!!!!!!!!!!!!!!!!!!!!!
+    ADD HERE: add it to the "listings" list of the user
+    */
+  });
+  itemSaved.catch( (err) => { 
+    console.log("COULD NOT CREATE USER!!!");
+    res.end("COULD NOT CREATE USER!!!");
+  });
+});
+
+
+
+console.log("here!!!");
+
+
+function addListingToUser(paramUsername, listingToAdd){
+  let bb = User.findOneAndUpdate(
+    {username : paramUsername},
+    {
+      $push: {listings : listingToAdd}
+    },
+    {strict: true}
+  );
+
+  bb.then(() =>{
+    console.log("here!!!");
+  })
+
+  console.log("\nthis is Users after the update: ");
+  printSchema("user");
+}
+
+
+
+function addPurchaseToUser(paramUsername, purchaseToAdd){
+  let bb = User.findOneAndUpdate(
+    {username : paramUsername},
+    {
+      $push: {purchases : purchaseToAdd}
+    },
+    {strict: true}
+  );
+
+  bb.then(() =>{
+    console.log("here!!!");
+  })
+
+  console.log("\nthis is Users after the update: ");
+  printSchema("user");
+}
+
+
+
+
+function printSchema(t){
+  /*
+  FUNCTION FOR TESTING
+
+  prints the schema
+  */
+  if ((t == "user")|| (t == "users") || (t==1) || (t==0)){
+    let q = User.find({});
+    q.then((documents) => {
+      console.log("[ ");
+      for (let i = 0; i < documents.length; i++){
+        console.log(documents[i]);
+      }
+      console.log(" ]");
+    } )
+  }
+  else{
+    //
+    let q = Item.find({});
+    q.then((documents) => {
+      console.log("[ ");
+      for (let i = 0; i < documents.length; i++){
+        console.log(documents[i]);
+      }
+      console.log(" ]");
+    } )
+  }
+}
+
+
+
 const port = 3000;
 app.listen(port, () => { console.log('server has started and is running on localhost %d', port); });
 
+
+console.log("item schema:");
+printSchema("ejhfkejfh");
+console.log("\n\nuser schema:");
+printSchema("user");
 
