56b33b51277e08f5f0755c74831ed5d29ce81280
j-ryan33 <148741424+j-ryan33@users.noreply.github.com>
2023-11-08 18:07:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const { use } = require('express/lib/application');
 // const connection = 'mongodb+srv://doadmin:94B6u27tbJX1P83Q@chatty-db-2b2a9a25.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=chatty-db';
 // const connection = 'mongodb://127.0.0.1/ostaa';
 // const connection = 'mongodb+srv://doadmin:94B6u27tbJX1P83Q@chatty-db-2b2a9a25.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=chatty-db';
-const connection = 'mongodb://127.0.0.1/ostaa';
+// const connection = 'mongodb://127.0.0.1/ostaa';
+const connection = 'mongodb+srv://doapps-01f70429-fac0-4ba6-83ed-369febb34dfd:H864cjg7BG91Mp32@chatty-db-2b2a9a25.mongo.ondigitalocean.com/admin?authSource=admin&tls=true';
 
 
 
